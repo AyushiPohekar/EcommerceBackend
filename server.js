15c58f384dcef385872629e93e32f371770ac254
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-04-02 01:58:42 +0100

    mongodb connected

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import colors from "colors";
 import morgan from "morgan";
 import connectDB from './config/db.js';
+import authRoutes from "./routes/authRoute.js";
+
 //configure env
 dotenv.config();
 
@@ -15,6 +17,10 @@ const app=express();
 //middlewares
 app.use(express.json())
 app.use(morgan('dev'))
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
 //rest api
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to ecommerce app</h1>")
