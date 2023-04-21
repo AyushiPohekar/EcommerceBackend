fdb8ff4af063bb8a42c4db057d0874e36c312423
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-04-22 00:55:26 +0100

    productdetails

--- controllers/productController.js
+++ controllers/productController.js
@@ -257,7 +257,7 @@ export const productListController = async (req, res) => {
 export const searchProductController = async (req, res) => {
   try {
     const { keyword } = req.params;
-    const resutls = await ProductModel
+    const results = await ProductModel
       .find({
         $or: [
           { name: { $regex: keyword, $options: "i" } },
@@ -265,7 +265,7 @@ export const searchProductController = async (req, res) => {
         ],
       })
       .select("-photo");
-    res.json(resutls);
+    res.json(results);
   } catch (error) {
     console.log(error);
     res.status(400).send({
